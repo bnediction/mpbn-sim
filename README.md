09c6416e714423d72143a05e99aa3640194cb3c6
Loïc Paulevé <loic.pauleve@ens-cachan.org>
2022-05-14 23:21:32 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# Simulation of Most Permissive Boolean networks
+
+## Installation
+
+```
+pip install https://github.com/bnediction/mpbn-sim/archive/refs/heads/main.zip
+```
+
+## Usage
+
+The `mpbn_sim` command takes as input a JSON file describing the model and the
+simulation parameters.
+See https://github.com/bnediction/mpbn-sim/tree/main/experiments
+for example of experiment files.
+
+
+```
+mpbn_sim experiments/file.json
+```
+
+See `mpbn_sim -h` for options.
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+NAME = "mpbn_sim"
+VERSION = "0.1"
+
+setup(name=NAME,
+    version=VERSION,
+    author="Loïc Paulevé",
+    author_email="loic.pauleve@labri.fr",
+    url="https://github.com/bnediction/mpbn-sim",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords='',
+    description="Simulation of Most Permissive Boolean networks",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "mpbn",
+        "numpy",
+        "scipy",
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "mpbn_sim=mpbn_sim.cli",
+        ],
+    },
+    packages=[NAME],
+)
